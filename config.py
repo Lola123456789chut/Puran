0aac7d25695335ce1482f67235f06c0c19d3528d
Lola123456789chut <pradeepsinghkangari@gmail.com>
2024-09-13 18:18:58 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY VENOMxCRAZY
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7339152820:AAEW3C6yL0gb_LytmC1xRu3qORHB5aKoFpQ")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("@PURAN_MAX_YT")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("6522852413")
+
+
+
+
+
+
+
+  
